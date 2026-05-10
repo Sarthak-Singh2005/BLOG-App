4f79376954fd03b995c031c27b58ec9c2a8f47c9
Sarthak-Singh2005 <sarthakkr.singh.06@gmail.com>
2026-05-10 13:12:18 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
-import { Link } from "react-router-dom";
-import { Route } from "react-router-dom";
-import { Routes } from "react-router-dom";
-import "./App.css"
-function Home(){
-  return(
-    <div>
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Blog from "./Components/Blog";
+import Createblog from "./Components/createBlog";
+import Editblog from "./Components/Editblog";
+function Home() {
+  return (
+    <div style={{ padding: "20px", color: "white" }}>
       <Link to="/register">
         <button className="Sign">Sign In</button>
       </Link>
@@ -16,16 +17,28 @@ function Home(){
       </Link>
       <h1>Blogging App</h1>
     </div>
-  )
+  );
 }
-export default function App(){
-  return(
-    <div>
+
+export default function App() {
+  return (
+    <div style={{ minHeight: "100vh", backgroundColor: "blueviolet" }}>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/register" element={<Register/>} />
-        <Route path="/login"  element={<Login/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/create" element={<Createblog/>}/>
+        <Route path="/edit" element={<Editblog/>}/>
+        <Route
+          path="*"
+          element={
+            <div style={{ padding: "40px", color: "white", fontSize: "24px" }}>
+              Page not found. Use /login to sign in.
+            </div>
+          }
+        />
       </Routes>
     </div>
-  )
+  );
 }
